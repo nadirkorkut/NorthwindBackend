6102c886518f74a59df960a1530b3ec4eb5dfdfd
nadirkorkut <korkutnadir@outlook.com>
2021-03-16 23:00:46 +0300

    Authentication Servisi yazıldı.

--- Business/Contants/Messages.cs
+++ Business/Contants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,8 +10,16 @@ namespace Business.Contants
         public static string ProductAdded = "Ürün Başarıyla Eklendi";
         public static string ProductDeleted = "Ürün Başarıyla Silindi";
         public static string ProductUpdated = "Ürün Başarıyla Güncellendi";
+
         public static string CategoryAdded = "Kategori Başarıyla Eklendi";
         public static string CategoryDeleted = "Kategori Başarıyla Silindi";
         public static string CategoryUpdated = "Kategori Başarıyla Güncellendi";
+
+        public static string UserNotFound="Kullanıcı Bulunamadı";
+        public static string PasswordError="Şifre Hatalı";
+        public static string SuccessfulLogin="Sisteme Giriş Başaralı";
+        public static string UserAlreadyExists="Bu Kullanıcı Mevcut";
+        public static string UserRegistered="Kullanıcı Başarıyla Kaydedildi";
+        public static string AccessTokenCreated="Access Token Başarıyla Oluşturuldu.";
     }
 }
